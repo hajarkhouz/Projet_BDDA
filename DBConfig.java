78735f4dd140f99a5b4c07daf5ceaf88148e391e
racha <racha2004chegg@gmail.com>
2025-09-30 10:59:44 +0200

    Initial commit with Java classes

---
+++ DBConfig.java
@@ -0,0 +1,13 @@
+public class DBConfig {
+    private String dbpath;
+
+    public DBConfig(String dbpath) {
+        this.dbpath = dbpath;
+
+    }
+
+    // public static config LoadDBConfig(fichier_config){
+
+    // }
+
+}

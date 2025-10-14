8d4ddc952cae81d41c743cb0de40d38887dff689
samy-benmeziane <samy.benmeziane@univ-bouira.dz>
2025-10-14 15:31:45 +0200

    feat: ajout des classes DBConfig et DiskManager avec leurs tests unitaires

--- src/DiskManager.java
+++ src/DiskManager.java
@@ -28,56 +28,7 @@ public class DiskManager {
         // Optionnel : sauvegarder les pages libres dans un fichier (dm.save)
     }
 
-    public PageId AllocPage() throws IOException {
-        if (!freePages.isEmpty()) {
-            Iterator<PageId> it = freePages.iterator();//cette methode  retourne un objet Iterator qui permet de parcourir les elements de l'ensemble freePages
-            PageId pid = it.next();//cette methode retourne le premier element disponible dans l'ensemble freePages
-            it.remove();
-            return pid;
-        }
-
-        File binDir = new File(dbConfig.getDbpath());//repertoire ou sont stockes les fichiers de donnees
-
-        for (int i = 0; i < dbConfig.getDmMaxFileCount(); i++) {
-            File file = new File(binDir, "Data" + i + ".rsdb");
-
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            long fileSize = file.length();//taille actuelle du fichier en octets
-            if (fileSize + dbConfig.getPagesize() <= dbConfig.getDmMaxFileSize()) {//verifie si l'ajout d'une nouvelle page ne depasse pas la taille maximale autorisee
-                try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {//ouvre le fichier en mode lecture ecriture
-                    raf.seek(fileSize);//positionne le curseur a la fin du fichier pour ajouter une nouvelle page
-                    raf.write(new byte[dbConfig.getPagesize()]);//ecrit des octets vides pour initialiser la nouvelle page
-                }
-                int pageIdx = (int) (fileSize / dbConfig.getPagesize());
-                return new PageId(i, pageIdx);
-            }
-        }
-
-        throw new IOException("Aucun fichier disponible pour l'allocation.");
-    }
-
-    public void DeallocPage(PageId pid) {
-        freePages.add(pid);
-    }
-
-    public void ReadPage(PageId pid, ByteBuffer buffer) throws IOException {
-        if (buffer.capacity() != dbConfig.getPagesize()) {
-            throw new IllegalArgumentException("Taille du buffer incorrecte !");
-        }
-
-        File file = new File(dbConfig.getDbpath(), "Data" + pid.getFileIdx() + ".rsdb");
-        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
-            raf.seek((long) pid.getPageIdx() * dbConfig.getPagesize());
-            byte[] data = new byte[dbConfig.getPagesize()];
-            raf.readFully(data); 
-            buffer.clear();
-            buffer.put(data);
-            buffer.flip();//prepare le buffer pour la lecture
-        }
-    }
+    
 
     public void WritePage(PageId pid, ByteBuffer buffer) throws IOException {
         if (buffer.capacity() != dbConfig.getPagesize()) {
